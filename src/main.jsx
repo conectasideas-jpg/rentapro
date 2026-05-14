55ccf5e48700c03899fb180bfa983e145a06e03e
ideas <conectasideas@gmail.com>
2026-05-13 23:38:59 -0400

    cambios de carga

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null }
+  }
+  componentDidCatch(error) {
+    this.setState({ error })
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ padding: 24, fontFamily: 'monospace', fontSize: 13 }}>
+          <h2 style={{ color: 'red' }}>Error al cargar la app</h2>
+          <pre style={{ background: '#f5f5f5', padding: 16, borderRadius: 8, whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
+            {this.state.error.message}
+            {'\n\n'}
+            {this.state.error.stack}
+          </pre>
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 )
